6068833199b78cf79f49d24a9977b147ff896744
janakiram11072002 <janakiram11072002@gmail.com>
2024-06-30 18:39:03 +0530

    Added AppConfig which store teh properties which are required in project form application.properties

---
+++ src/main/java/com/jhonny/SharePointReports/Utils/AppConfig.java
@@ -0,0 +1,55 @@
+package com.jhonny.SharePointReports.Utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+//@Configuration()
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+@PropertySource("classpath:application.properties")
+public class AppConfig
+{
+    @Value("${app.config.tenantid}")
+    private String tenantid;
+    @Value("${app.config.tenantname}")
+    private String tenantname;
+    @Value("${app.config.certificatepath}")
+    private String certificatepath;
+    @Value("${app.config.certificatekey}")
+    private String certificatekey;
+    @Value("${app.config.clientid}")
+    private String clientid;
+
+    public String getTenatId() {
+        return tenantid;
+    }
+
+    public String getTenantName() {
+        return tenantname;
+    }
+
+    public String getCertPath() {
+        return certificatepath;
+    }
+
+    public String getCertKey() {
+        return certificatekey;
+    }
+
+    public String getClientId(){
+        return clientid;
+    }
+
+    /*@Bean(value = "AppConfig")
+    public AppConfig AppConfig()
+    {
+        return this;
+    }*/
+}
