f24bb1d3c8c9f319620d0501bf11bd29002229a1
Demodroid <demodemo9688@gmail.com>
2024-07-03 18:48:06 +0530

    added prepare body method to prepare body

--- src/main/java/com/jhonny/SharePointReports/Utils/AuthClient.java
+++ src/main/java/com/jhonny/SharePointReports/Utils/AuthClient.java
@@ -1,6 +1,9 @@
 package com.jhonny.SharePointReports.Utils;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,6 +11,7 @@ public class AuthClient
 {
     final private String clientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
     final private String authUrl = "https://login.microsoftonline.com/%s/oauth2/v2.0/token";
+    final private String bodyPattern = "\"grant_type=client_credentials&client_id=%s&client_assertion=%s&client_assertion_type=%s&scope=%s";
     private String tenant_id, client_id, client_assertion, scope;
     private AppConfig appConfig;
 
@@ -17,7 +21,7 @@ public class AuthClient
 //        this.client_id = client_id;
         this.appConfig = new AnnotationConfigApplicationContext(AppConfig.class).getBean(AppConfig.class);
         this.client_assertion = new JwtGenerater().getJwtToken();
-        this.scope = scope;
+        this.scope = scope+"/.default";
     }   
 
     private String prepareUrl()
@@ -25,21 +29,38 @@ public class AuthClient
         String url = String.format(this.authUrl, appConfig.getTenatId());
         return url;
     }
-
-    public String prepareBody()
+    public Map<String, String> prepareBodyAsForm()
     {
-        String body = String.format("{\"client_id\":\"%s\", "+
-                        "\"grant_type\":\"client_credentials\", "+
-                        "\"client_assertion\":\"%s\", "+
-                        "\"client_assertion_type\":\"%s\", "+
-                        "\"scope\":\"%s\"}",
-                        appConfig.getClientId(), this.client_assertion, this.clientAssertionType, this.scope
-                         );
+        Map<String, String> formData = new HashMap<>();
+        formData.put("grant_type","client_credentials");
+        formData.put("client_id", client_id);
+        formData.put("client_assertion",client_assertion);
+        formData.put("client_assertion_type",clientAssertionType);
+        formData.put("scope",scope);
+        return formData;
+    }
+    public String prepareBodyAsURLEncoder()
+    {
+        String body = String.format(bodyPattern,client_id, client_assertion, clientAssertionType,scope);
+        return body;
+    }
+    public String prepareBodyAsJson()
+    {
+        String body = String.format("{\n"+
+                "\"grant_type\":\"client_credentials\", "+
+                "\"client_id\":\"%s\", "+
+                "\"client_assertion\":\"%s\", "+
+                "\"client_assertion_type\":\"%s\", "+
+                "\"scope\":\"%s\"\n}",
+                appConfig.getClientId(), 
+                this.client_assertion, 
+                this.clientAssertionType, 
+                this.scope);
         return body;
     }
     public String getAccessToken()
     {
-        String tokenResponse = new HttpUtils().Post(prepareUrl(), prepareBody()).body();
+        String tokenResponse = new HttpUtils().Post(prepareUrl(), prepareBodyAsForm()).body();
         //to parse the response object and get the token form it.
         return tokenResponse.toString();
     }
