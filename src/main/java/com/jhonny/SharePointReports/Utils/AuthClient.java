d6a60a5b6d2955c8b25a1db3bd752a3197735a45
Demodroid <demodemo9688@gmail.com>
2024-07-04 15:01:04 +0530

    Removed unwanted method.

--- src/main/java/com/jhonny/SharePointReports/Utils/AuthClient.java
+++ src/main/java/com/jhonny/SharePointReports/Utils/AuthClient.java
@@ -4,18 +4,17 @@ package com.jhonny.SharePointReports.Utils;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AuthClient
 {
     final private String clientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
     final private String authUrl = "https://login.microsoftonline.com/%s/oauth2/v2.0/token";
-    final private String bodyPattern = "\"grant_type=client_credentials&client_id=%s&client_assertion=%s&client_assertion_type=%s&scope=%s";
-    private String tenant_id, client_id, client_assertion, scope;
+    //final private String bodyPattern = "\"grant_type=client_credentials&client_id=%s&client_assertion=%s&client_assertion_type=%s&scope=%s";
+    private String client_assertion, scope;
     private AppConfig appConfig;
 
-    public AuthClient(/*String tenantId, String client_id, String client_assertion,*/ String scope)
+    public AuthClient(String scope)
     {
 //        this.tenant_id = tenantId;
 //        this.client_id = client_id;
@@ -33,31 +32,31 @@ public class AuthClient
     {
         Map<String, String> formData = new HashMap<>();
         formData.put("grant_type","client_credentials");
-        formData.put("client_id", client_id);
+        formData.put("client_id", appConfig.getClientId());
         formData.put("client_assertion",client_assertion);
         formData.put("client_assertion_type",clientAssertionType);
         formData.put("scope",scope);
         return formData;
     }
-    public String prepareBodyAsURLEncoder()
-    {
-        String body = String.format(bodyPattern,client_id, client_assertion, clientAssertionType,scope);
-        return body;
-    }
-    public String prepareBodyAsJson()
-    {
-        String body = String.format("{\n"+
-                "\"grant_type\":\"client_credentials\", "+
-                "\"client_id\":\"%s\", "+
-                "\"client_assertion\":\"%s\", "+
-                "\"client_assertion_type\":\"%s\", "+
-                "\"scope\":\"%s\"\n}",
-                appConfig.getClientId(), 
-                this.client_assertion, 
-                this.clientAssertionType, 
-                this.scope);
-        return body;
-    }
+    // public String prepareBodyAsURLEncoder()
+    // {
+    //     String body = String.format(bodyPattern,AppConfig.getClientId(), client_assertion, clientAssertionType,scope);
+    //     return body;
+    // }
+    // public String prepareBodyAsJson()
+    // {
+    //     String body = String.format("{\n"+
+    //             "\"grant_type\":\"client_credentials\", "+
+    //             "\"client_id\":\"%s\", "+
+    //             "\"client_assertion\":\"%s\", "+
+    //             "\"client_assertion_type\":\"%s\", "+
+    //             "\"scope\":\"%s\"\n}",
+    //             appConfig.getClientId(), 
+    //             this.client_assertion, 
+    //             this.clientAssertionType, 
+    //             this.scope);
+    //     return body;
+    // }
     public String getAccessToken()
     {
         String tokenResponse = new HttpUtils().Post(prepareUrl(), prepareBodyAsForm()).body();
