820bb4294f6cebc483a6295b7b89242dc2808a91
janakiram11072002 <janakiram11072002@gmail.com>
2024-07-18 23:10:52 +0530

    Removed unused imports

--- src/main/java/com/jhonny/SharePointReports/Utils/AppConfig.java
+++ src/main/java/com/jhonny/SharePointReports/Utils/AppConfig.java
@@ -2,9 +2,6 @@ package com.jhonny.SharePointReports.Utils;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
