58281dc050ff523732443a2e153573bb4f2ed51b
Demodroid <demodemo9688@gmail.com>
2024-07-18 11:46:51 +0530

    Added SiteAudit class

---
+++ src/main/java/com/jhonny/SharePointReports/PersistenceModels/MetaData_Objects/StieAudit.java
@@ -0,0 +1,20 @@
+package com.jhonny.SharePointReports.PersistenceModels.MetaData_Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StieAudit 
+{
+    @JsonProperty("AuditFlags")
+    private int AuditFlags;
+
+    public int getAuditFlags() {
+        return AuditFlags;
+    }
+
+    public void setAuditFlags(int auditFlags) {
+        AuditFlags = auditFlags;
+    }
+    
+}
