b6dd763cd855b66512c13abf8349845f60634720
janakiram11072002 <janakiram11072002@gmail.com>
2024-07-21 22:41:58 +0530

    Added Response to the web class to generate web pojo class.

--- src/main/java/com/jhonny/SharePointReports/PersistenceModels/MetaData_Objects/WebProperties/WebProperties.java
+++ src/main/java/com/jhonny/SharePointReports/PersistenceModels/MetaData_Objects/WebProperties/WebProperties.java
@@ -4,5 +4,61 @@ public class WebProperties {
 }
 
 /*
-
- */
+    {
+        "AllowRssFeeds": true,
+        "AlternateCssUrl": "",
+        "AppInstanceId": "00000000-0000-0000-0000-000000000000",
+        "ClassicWelcomePage": null,
+        "Configuration": 0,
+        "Created": "2023-11-25T12:25:04.757",
+        "CurrentChangeToken": {
+            "StringValue": "1;2;2ff69ca1-2187-472d-9315-708235bb9c15;638571409112970000;385943427"
+        },
+        "CustomMasterUrl": "/sites/1gate/_catalogs/masterpage/seattle.master",
+        "Description": "for testing permission matrix in SharegateProduct.",
+        "DesignPackageId": "00000000-0000-0000-0000-000000000000",
+        "DocumentLibraryCalloutOfficeWebAppPreviewersDisabled": false,
+        "EnableMinimalDownload": false,
+        "FooterEmphasis": 0,
+        "FooterEnabled": false,
+        "FooterLayout": 0,
+        "HeaderEmphasis": 0,
+        "HeaderLayout": 2,
+        "HideTitleInHeader": false,
+        "HorizontalQuickLaunch": false,
+        "Id": "2ff69ca1-2187-472d-9315-708235bb9c15",
+        "IsEduClass": false,
+        "IsEduClassProvisionChecked": false,
+        "IsEduClassProvisionPending": false,
+        "IsHomepageModernized": false,
+        "IsMultilingual": true,
+        "IsRevertHomepageLinkHidden": false,
+        "Language": 1033,
+        "LastItemModifiedDate": "2024-07-16T13:57:10Z",
+        "LastItemUserModifiedDate": "2024-05-16T10:53:14Z",
+        "LogoAlignment": 0,
+        "MasterUrl": "/sites/1gate/_catalogs/masterpage/seattle.master",
+        "MegaMenuEnabled": false,
+        "NavAudienceTargetingEnabled": false,
+        "NoCrawl": false,
+        "ObjectCacheEnabled": false,
+        "OverwriteTranslationsOnChange": false,
+        "ResourcePath": {
+            "DecodedUrl": "https://42jghx.sharepoint.com/sites/1gate"
+        },
+        "QuickLaunchEnabled": true,
+        "RecycleBinEnabled": true,
+        "SearchScope": 0,
+        "ServerRelativeUrl": "/sites/1gate",
+        "SiteLogoUrl": "/sites/1gate/_api/GroupService/GetGroupImage?id='91a5d335-8d68-4633-a13f-3029deb30240'&hash=638379705452068262",
+        "SyndicationEnabled": true,
+        "TenantAdminMembersCanShare": 0,
+        "Title": "1gate",
+        "TreeViewEnabled": false,
+        "UIVersion": 15,
+        "UIVersionConfigurationEnabled": false,
+        "Url": "https://42jghx.sharepoint.com/sites/1gate",
+        "WebTemplate": "GROUP",
+        "WelcomePage": "SitePages/CollabHome.aspx"
+    }
+*/
