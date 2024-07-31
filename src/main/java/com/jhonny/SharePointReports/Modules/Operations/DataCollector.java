5a278b65a9fd5d182d2ffbe813ae53b6659af29a
janakiram11072002 <janakiram11072002@gmail.com>
2024-07-31 23:15:49 +0530

    collected site properties and Tenant siteproperties and populated data

--- src/main/java/com/jhonny/SharePointReports/Modules/Operations/DataCollector.java
+++ src/main/java/com/jhonny/SharePointReports/Modules/Operations/DataCollector.java
@@ -48,7 +48,7 @@ public class DataCollector
         int currentRow=0, end=0;
         do
         {
-            String response = new HttpUtils().Get(url, new AuthClient().GetAdminToken(), prepSiteBody(currentRow)).body();
+            String response = new HttpUtils().Get(url, new AuthClient().GetAdminToken(), prepAdminSiteBody(currentRow)).body();
             SiteUrlFromAdmin sites = JsonUtils.toObject(response, SiteUrlFromAdmin.class);
             for(Row site : sites.getRows())
             {
@@ -77,7 +77,7 @@ public class DataCollector
         int currentRow=0, end=0;
         do
         {
-            String response = new HttpUtils().Get(url, new AuthClient().GetAdminToken(), prepWebBody(currentRow)).body();
+            String response = new HttpUtils().Get(url, new AuthClient().GetAdminToken(), prepAdminWebBody(currentRow)).body();
             SiteUrlFromAdmin sites = JsonUtils.toObject(response, SiteUrlFromAdmin.class);
             for(Row site : sites.getRows())
             {
@@ -99,7 +99,7 @@ public class DataCollector
         }
         while(currentRow < end);
     }
-    private Map<String, String> prepSiteBody(int startInd)
+    private Map<String, String> prepAdminSiteBody(int startInd)
     {
         /*
         https://42jghx-admin.sharepoint.com/_api/search/query
@@ -118,7 +118,7 @@ public class DataCollector
         return body;
 
     }
-    private Map<String, String> prepWebBody(int startInd)
+    private Map<String, String> prepAdminWebBody(int startInd)
     {
         /*
         https://42jghx-admin.sharepoint.com/_api/search/querydd
@@ -137,13 +137,29 @@ public class DataCollector
         return body;
 
     }
+    private Map<String, String> getSiteQuery()
+    {
+        /*
+        select=AllowExternalEmbeddingWrapper,AllowedExternalDomains,CanSyncHubSitePermissions,CanUpgrade,CommentsOnSitePagesDisabled,ComplianceAttribute,CustomizedFormsPages,ExternalUserExpirationInDays,RelatedGroupId,SearchBoxInNavBar,SearchBoxPlaceholderText,SensitivityLabelId,ShowPeoplePickerSuggestionsForGuestUsers,SitePolicyEnabled,SocialBarOnSitePagesDisabled,StatusBarLink,StatusBarText,ThicketSupportDisabled,RelatedGroupId
+        &
+        expand=Owner,CustomScriptSafeDomains,SensitivityLabelInfo,ServerRelativePath,Usage,UpgradeInfo,Audit,SecondaryContact
+
+         */
+        Map<String, String> query = new HashMap<>();
+        query.put("expand","Owner,CustomScriptSafeDomains,SensitivityLabelInfo,ServerRelativePath,Usage,UpgradeInfo,Audit,SecondaryContact");
+        query.put("select","AllowExternalEmbeddingWrapper,AllowedExternalDomains,CanSyncHubSitePermissions,CanUpgrade,CommentsOnSitePagesDisabled,ComplianceAttribute,CustomizedFormsPages,ExternalUserExpirationInDays,RelatedGroupId,SearchBoxInNavBar,SearchBoxPlaceholderText,SensitivityLabelId,ShowPeoplePickerSuggestionsForGuestUsers,SitePolicyEnabled,SocialBarOnSitePagesDisabled,StatusBarLink,StatusBarText,ThicketSupportDisabled,RelatedGroupId");
+        return query;
+    }
+
     public void getSiteProperties(String url)
     {
         String endpoint = url+"/_api/site";
-        String response = new HttpUtils()
-            .Get(endpoint, new AuthClient().GetSiteClientToken(),null)
+        String siteResponse = new HttpUtils()
+            .Get(endpoint, new AuthClient().GetSiteClientToken(),getSiteQuery())
             .body();
-        Site site = new Site(JsonUtils.toObject(response, SiteProperties.class));
+        Site site = new Site(JsonUtils.toObject(siteResponse, SiteProperties.class));
+        site.updateSiteProp(getSitePropertiesV2(appConfig.getTenantAdminSite(),url));
+
 
         //Site Owner Details
 //        response = new HttpUtils()
@@ -170,24 +186,29 @@ public class DataCollector
 //            .body();
 //        site.updateAudit(JsonUtils.toObject(response, SiteAudit.class));
 
-        System.out.println(response);
+        System.out.println(site);
         //Site Audit
     }
 
-    public String getSitePropertiesV2(String url)
+    public TenantLevelSiteProperties getSitePropertiesV2(String adminSite, String url)
     {
-        String endpoint = url+"/_vti_bin/client.svc/ProcessQuery";
-        String body = "<Request AddExpandoFieldTypeSuffix=\"true\" SchemaVersion=\"15.0.0.0\" LibraryVersion=\"16.0.0.0\" ApplicationName=\".NET Library\" xmlns=\"http://schemas.microsoft.com/sharepoint/clientquery/2009\"><Actions><ObjectPath Id=\"2\" ObjectPathId=\"1\" /><ObjectPath Id=\"4\" ObjectPathId=\"3\" /><Query Id=\"5\" ObjectPathId=\"3\"><Query SelectAllProperties=\"false\"><Properties><Property Name=\"AllowCreateDeclarativeWorkflow\" ScalarProperty=\"true\" /><Property Name=\"AllowDesigner\" ScalarProperty=\"true\" /><Property Name=\"AllowMasterPageEditing\" ScalarProperty=\"true\" /><Property Name=\"AllowRevertFromTemplate\" ScalarProperty=\"true\" /><Property Name=\"AllowSaveDeclarativeWorkflowAsTemplate\" ScalarProperty=\"true\" /><Property Name=\"AllowSavePublishDeclarativeWorkflow\" ScalarProperty=\"true\" /><Property Name=\"AllowSelfServiceUpgrade\" ScalarProperty=\"true\" /><Property Name=\"AllowSelfServiceUpgradeEvaluation\" ScalarProperty=\"true\" /><Property Name=\"Audit\"><Query SelectAllProperties=\"false\"><Properties><Property Name=\"AuditFlags\" ScalarProperty=\"true\" /></Properties></Query></Property><Property Name=\"AuditLogTrimmingRetention\" ScalarProperty=\"true\" /><Property Name=\"CanUpgrade\" ScalarProperty=\"true\" /><Property Name=\"Classification\" ScalarProperty=\"true\" /><Property Name=\"CompatibilityLevel\" ScalarProperty=\"true\" /><Property Name=\"DisableAppViews\" ScalarProperty=\"true\" /><Property Name=\"DisableCompanyWideSharingLinks\" ScalarProperty=\"true\" /><Property Name=\"DisableFlows\" ScalarProperty=\"true\" /><Property Name=\"ExternalSharingTipsEnabled\" ScalarProperty=\"true\" /><Property Name=\"ExternalUserExpirationInDays\" ScalarProperty=\"true\" /><Property Name=\"GroupId\" ScalarProperty=\"true\" /><Property Name=\"Id\" ScalarProperty=\"true\" /><Property Name=\"LockIssue\" ScalarProperty=\"true\" /><Property Name=\"MaxItemsPerThrottledOperation\" ScalarProperty=\"true\" /><Property Name=\"NeedsB2BUpgrade\" ScalarProperty=\"true\" /><Property Name=\"Owner\"><Query SelectAllProperties=\"false\"><Properties><Property Name=\"LoginName\" ScalarProperty=\"true\" /><Property Name=\"Email\" ScalarProperty=\"true\" /><Property Name=\"Title\" ScalarProperty=\"true\" /></Properties></Query></Property><Property Name=\"PrimaryUri\" ScalarProperty=\"true\" /><Property Name=\"ReadOnly\" ScalarProperty=\"true\" /><Property Name=\"RequiredDesignerVersion\" ScalarProperty=\"true\" /><Property Name=\"SandboxedCodeActivationCapability\" ScalarProperty=\"true\" /><Property Name=\"SecondaryContact\"><Query SelectAllProperties=\"false\"><Properties><Property Name=\"LoginName\" ScalarProperty=\"true\" /><Property Name=\"Email\" ScalarProperty=\"true\" /><Property Name=\"Title\" ScalarProperty=\"true\" /></Properties></Query></Property><Property Name=\"SensitivityLabel\" ScalarProperty=\"true\" /><Property Name=\"SensitivityLabelId\" ScalarProperty=\"true\" /><Property Name=\"SensitivityLabelInfo\" ScalarProperty=\"true\" /><Property Name=\"ServerRelativeUrl\" ScalarProperty=\"true\" /><Property Name=\"ShareByEmailEnabled\" ScalarProperty=\"true\" /><Property Name=\"ShareByLinkEnabled\" ScalarProperty=\"true\" /><Property Name=\"ShowPeoplePickerSuggestionsForGuestUsers\" ScalarProperty=\"true\" /><Property Name=\"ShowUrlStructure\" ScalarProperty=\"true\" /><Property Name=\"StatusBarLink\" ScalarProperty=\"true\" /><Property Name=\"StatusBarText\" ScalarProperty=\"true\" /><Property Name=\"TrimAuditLog\" ScalarProperty=\"true\" /><Property Name=\"UIVersionConfigurationEnabled\" ScalarProperty=\"true\" /><Property Name=\"UpgradeInfo\" ScalarProperty=\"true\" /><Property Name=\"UpgradeReminderDate\" ScalarProperty=\"true\" /><Property Name=\"UpgradeScheduled\" ScalarProperty=\"true\" /><Property Name=\"UpgradeScheduledDate\" ScalarProperty=\"true\" /><Property Name=\"Upgrading\" ScalarProperty=\"true\" /><Property Name=\"Url\" ScalarProperty=\"true\" /><Property Name=\"Usage\" ScalarProperty=\"true\" /></Properties></Query></Query></Actions><ObjectPaths><StaticProperty Id=\"1\" TypeId=\"{3747adcd-a3c3-41b9-bfab-4a64dd2f1e0a}\" Name=\"Current\" /><Property Id=\"3\" ParentId=\"1\" Name=\"Site\" /></ObjectPaths></Request>\n";
-        String response = (url.contains("-my.sharepoint.com")) ?
-                (new HttpUtils().PostAsXML(endpoint, new AuthClient().GetOneDriveClientToken(),body).body()) :
-                (new HttpUtils().PostAsXML(endpoint, new AuthClient().GetSiteClientToken(),body).body());
+        String endpoint = adminSite+"/_vti_bin/client.svc/ProcessQuery";
+        String response = new HttpUtils()
+                .PostAsXML(endpoint,
+                        new AuthClient().GetAdminToken(),
+                        String.format(getTenantSitePropXml(),url,appConfig.getTenatId()))
+                .body();
         System.out.println(response);
-        //Site site = new Site(JsonUtils.toObject(response, SiteProperties.class));
-        return response;
+        String breakResponse = response.split("},1,")[1];
+        TenantLevelSiteProperties siteProp = (JsonUtils.toObject(breakResponse.substring(0,breakResponse.length()-2), TenantLevelSiteProperties.class));
+        return siteProp;
 
         //Site Audit
     }
-
+    private String getTenantSitePropXml()
+    {
+        return "<Request AddExpandoFieldTypeSuffix=\"true\" SchemaVersion=\"15.0.0.0\" LibraryVersion=\"16.0.0.0\" ApplicationName=\".NET Library\" xmlns=\"http://schemas.microsoft.com/sharepoint/clientquery/2009\"><Actions><Query Id=\"1\" ObjectPathId=\"12\"><Query SelectAllProperties=\"true\"><Properties/></Query></Query></Actions><ObjectPaths><Method Id=\"12\" ParentId=\"1\" Name=\"GetSitePropertiesByUrl\"><Parameters><Parameter Type=\"String\">%s</Parameter><Parameter Type=\"Boolean\"></Parameter></Parameters></Method><Identity Id=\"1\" Name=\"9f0341a1-90f2-3000-9074-7131eb48af00|908bed80-a04a-4433-b4a0-883d9847d110:%s&#xA;Tenant\"/></ObjectPaths></Request>";
+    }
     public void getWebProperties(String url)
     {
 
