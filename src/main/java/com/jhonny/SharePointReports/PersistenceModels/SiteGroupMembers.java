4ffde604f204b671a63e7a88b310c834cdc01ce2
janakiram11072002 <janakiram11072002@gmail.com>
2024-08-05 22:19:47 +0530

    Added Site Group Members properties.

---
+++ src/main/java/com/jhonny/SharePointReports/PersistenceModels/SiteGroupMembers.java
@@ -0,0 +1,26 @@
+package com.jhonny.SharePointReports.PersistenceModels;
+
+public class SiteGroupMembers
+{
+    private int siteType;
+    private String siteId;
+    private String siteUrl;
+    private String siteTitle;
+    private String webId;
+    private String webUrl;
+    private String webTitle;
+    private int groupId;
+    private String groupTitle;
+    private int id;
+    private String title;
+    private String email;
+    private String loginName;
+    private String domainName;
+    private boolean isExternalUser;
+    private boolean isSystemAccount;
+    private boolean isSiteAdmin;
+    private boolean isOwner;
+    private boolean isSiteMember;
+    private boolean isSiteVisitor;
+    private int principalType;
+}
