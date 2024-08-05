2502a4f15e76e588af182dd991653e35983d355d
janakiram11072002 <janakiram11072002@gmail.com>
2024-08-05 22:18:05 +0530

    Added Site Users properties

---
+++ src/main/java/com/jhonny/SharePointReports/PersistenceModels/SiteUsers.java
@@ -0,0 +1,26 @@
+package com.jhonny.SharePointReports.PersistenceModels;
+
+public class SiteUsers
+{
+    private int siteType;
+    private String siteId;
+    private String webId;
+    private String email;
+    private String groups;
+    private boolean isShareByEmailGuestUser;
+    private boolean isSiteAdmin;
+    private boolean isSiteOwner;
+    private String userId_nameId;
+    private String userId_nameIdUser;
+    private String userId_typeId;
+    private int id;
+    private boolean isHiddenInUI;
+    private String loginName;
+    private String title;
+    private int principalType;
+    private String objectVersion;
+    private String webUrl;
+    private String webTitle;
+    private String siteUrl;
+    private String siteTitle;
+}
