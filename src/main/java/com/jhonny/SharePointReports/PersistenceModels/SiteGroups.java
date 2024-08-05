b9d9a8dcde3718bf3a813192a0e621ddf1546058
janakiram11072002 <janakiram11072002@gmail.com>
2024-08-05 22:19:34 +0530

    Added Site Groups properties.

--- src/main/java/com/jhonny/SharePointReports/PersistenceModels/SiteGroups.java
+++ src/main/java/com/jhonny/SharePointReports/PersistenceModels/SiteGroups.java
@@ -1,4 +1,28 @@
 package com.jhonny.SharePointReports.PersistenceModels;
 
-public class SiteGroups {
+public class SiteGroups
+{
+    private int siteType;
+    private String siteId;
+    private String siteUrl;
+    private String siteTitle;
+    private String webId;
+    private String webUrl;
+    private String webTitle;
+    private boolean allowMembersEditMembership;
+    private boolean allowRequestToJoinLeave;
+    private boolean autoAcceptRequestToJoinLeave;
+    private boolean canCurrentUserEditMembership;
+    private boolean canCurrentUserManageGroup;
+    private boolean canCurrentUserViewMembership;
+    private String description;
+    private boolean onlyAllowMembersViewMembership;
+    private String ownerTitle;
+    private String requestToJoinLeaveEmailSetting;
+    private int memberCount;
+    private int id;
+    private boolean isHiddenInUI;
+    private String loginName;
+    private String title;
+    private int principalType;
 }
