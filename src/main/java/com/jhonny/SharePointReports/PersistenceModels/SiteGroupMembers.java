6df1870f909ac338f80772825b1f2bec73f91598
janakiram11072002 <janakiram11072002@gmail.com>
2024-08-11 20:40:36 +0530

    Added Data Mapping and manipulate data

--- src/main/java/com/jhonny/SharePointReports/PersistenceModels/SiteGroupMembers.java
+++ src/main/java/com/jhonny/SharePointReports/PersistenceModels/SiteGroupMembers.java
@@ -1,5 +1,7 @@
 package com.jhonny.SharePointReports.PersistenceModels;
 
+import com.jhonny.SharePointReports.PersistenceModels.MetaData_Objects.WebProperties.User;
+
 public class SiteGroupMembers
 {
     public int siteType;
@@ -23,4 +25,33 @@ public class SiteGroupMembers
     public boolean isSiteMember;
     public boolean isSiteVisitor;
     public int principalType;
+
+//    public SiteGroupMembers()
+//    {
+//
+//    }
+    public SiteGroupMembers(SiteGroups groupSource, User source)
+    {
+        this.siteType = groupSource.siteType;
+        this.siteId = groupSource.siteId;
+        this.siteUrl = groupSource.siteUrl;
+        this.siteTitle = groupSource.siteTitle;
+        this.webId = groupSource.webId;
+        this.webUrl = groupSource.webUrl;
+        this.webTitle = groupSource.webTitle;
+        this.groupId = groupSource.id;
+        this.groupTitle = groupSource.title;
+        this.id = source.getId();
+        this.title = source.getTitle();
+        this.email = source.getEmail();
+        this.loginName = source.getLoginName();
+//        this.domainName = source.;
+//        this.isExternalUser = source;
+//        this.isSystemAccount = source;
+        this.isSiteAdmin = source.isSiteAdmin();
+//        this.isOwner = source;
+//        this.isSiteMember = source;
+//        this.isSiteVisitor = source;
+//        this.principalType = source;
+    }
 }
