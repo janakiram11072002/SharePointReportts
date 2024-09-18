db1bd9e8e060e482a7f44a66dbf91eece972ca01
demodemo9688 <demodemo9688@gmail.com>
2024-09-18 09:33:45 +0000

    manually omplemented Entity manager for data

---
+++ src/main/java/com/jhonny/SharePointReports/Services/IEntity_DAO.java
@@ -0,0 +1,11 @@
+// package com.jhonny.SharePointReports.Services;
+
+// import java.util.List;
+
+// /**
+//  * IDAO
+//  */
+// public interface IEntity_DAO<T>
+// {
+//     void save(T entity);
+// }
